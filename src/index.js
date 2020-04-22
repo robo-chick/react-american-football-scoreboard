fa5b03bb72bfb56d12a3c5728249cd9110912796
Tasha <tashingsworth@gmail.com>
2020-04-22 12:13:32 -0400

    starting project, set up state values for Lions and Tigers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState}  from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+const App = () => {
+
+    const [lions, setLions] = useState(0);
+    const [tigers, setTigers] = useState(0);
+       
+
+
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
